dae4b7bc67f6356fd8566b0d31dad9e659c5ff03
kmtrrdev <168581428+kmtrrdev@users.noreply.github.com>
2024-05-10 17:05:07 +0300

    index.js

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var swiperx = new Swiper("#home", {
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    }
   }); 
 // farah
   var swiper = new Swiper(".mySwiper", {
